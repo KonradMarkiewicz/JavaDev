ba2adbe97a210df48f86a40637fc84458141b7f9
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-03-15 22:18:38 +0100

    Java - zadanie 4 - mapa z krajami

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,9 +4,7 @@ import com.company.creatures.Animal;
 import com.company.creatures.Human;
 import com.company.database.Connector;
 
-import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
 
@@ -36,10 +34,23 @@ public class Main {
         cars.put("Enzo","Ferrari");
         cars.put("Gallardo","Lamborgini");
 
+        System.out.println("Cars in created map ordered by models ascending:");
         for(String car : cars.keySet()){
             System.out.println(car);
         }
 
+        Map<String, Integer> countriesSize = new HashMap<>();
+        countriesSize.put("Poland",312696);
+        countriesSize.put("Germany",357578);
+        countriesSize.put("Belarus",207595);
+        countriesSize.put("Czech Republic",78866);
+        countriesSize.put("Slovakia",49035);
+
+        int maxSize = Collections.max(countriesSize.values());
+        System.out.println( "Largest size in countriesSize HashHap is: " + maxSize);
+
+        int minSize = Collections.min(countriesSize.values());
+        System.out.println("Smallest size in countriesSize HashHap is: " + minSize);
     }
 
 }
